f54499a7390c84cd816c01597c7bf017e92c2b30
hyukjung <hyukjung1013@gmail.com>
2019-05-06 15:33:27 +0900

    2nd Commit: (1) ejs template added. (views/*.ejs)

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const pageRouter = require('./routes/page');
 
